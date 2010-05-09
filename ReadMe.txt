65b9f4420f30788b5dce22b6920b2b5995d7c37d
kos59125 <devnull@localhost>
2010-05-09 23:12:33 +0900

    MIT License にした。 ReadMe およびサンプルコードの追加。

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+● 名前
+R2ch
+
+● 概要
+R コンソール上で２ちゃんねるを対話的に読むことを可能にします。
+
+● 配布ライセンス
+MIT License
+License.txt を参照してください。
+
+● URI
+http://project.recyclebin.jp/repos/junk/r2ch/
---
+++ Sample/sample.R
@@ -0,0 +1,21 @@
+## Ensure you have already read all sources in R2ch/R.
+# f <- list.files();  # in the source directory.
+# for (i in 1 : length(f)) source(f[i]);
+
+# 板一覧を取得。
+bbs <- r2ch.getBoardList();
+head(bbs);
+
+# Windows 板のスレッド情報を取得。
+win <- r2ch.getSubjectList(bbs[bbs$Name == "Windows", "URI"]);
+head(win);
+
+# Windows 板の 10 番目のスレッドを取得。
+thread <- r2ch.getThread(win[10, "DAT"]);
+head(thread);
+
+# 取得したスレッドを 3 レスずつ出力。
+r2ch.printThread(thread, by = 3);
+# [Enter] で次レス表示。
+# ? + [Enter] でヘルプ表示。
+# q + [Enter] で中断。
